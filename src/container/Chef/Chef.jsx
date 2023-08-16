fccf994968df0e1bc69b20f4c7c85e1bdb0a1a4f
Denis <denisnica.242004@gmail.com>
2023-08-16 16:59:16 -0600

    Chef y Intro seccion es terminada

--- src/container/Chef/Chef.jsx
+++ src/container/Chef/Chef.jsx
@@ -1,11 +1,30 @@
-import React from 'react';
-
+import { images } from '../../constants';
+import { SubHeading } from '../../components';
 import './Chef.css';
 
 const Chef = () => (
-  <div>
-    Chef
-  </div>
+  <section className='app__bg app__wrapper section__padding'>
+    <div className='app__wrapper_img app__wrapper_img-reverse'>
+      <img src={images.chef} alt="chef" />
+    </div>
+    <div className='app__wrapper_info'>
+      <SubHeading title="Chef's Word" />
+      <p>What we believe in</p>
+      <div className='app__chef-content'>
+        <div className='app__chef-content_quote'>
+          <img src={images.quote} alt="Quote" />
+          <p className='p__opensans'>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+        <p className='p__opensans'>Voluptate ratione, hic, quo, dolorem ipsam magni dicta id odit nam culpa quam animi. Voluptate, ullam et laudantium blanditiis vero maiores deserunt.</p>
+      </div>
+      <div className='app__chef-sing'>
+        <p>Kevin Luo</p>
+        <p className='P__opensans'>Chef & Founder</p>
+        <img src={images.sign} alt="Sign" />
+      </div>
+    </div>
+  </section>
 );
 
 export default Chef;
+ 
--- src/container/Intro/Intro.jsx
+++ src/container/Intro/Intro.jsx
@@ -1,11 +1,45 @@
-import React from 'react';
-
+import { BsFillPlayFill, BsPauseFill } from 'react-icons/bs';
+import { meal } from '../../constants';
 import './Intro.css';
+import { useRef, useState } from 'react';
 
-const Intro = () => (
-  <div>
-    Intro
-  </div>
-);
+const Intro = () => {
+  const [playVideo, setPlayVideo] = useState(false);
+  const videoRef = useRef(null);
+
+  const handleVideo = () => {
+    setPlayVideo(prev => !prev);
+
+    if (playVideo) {
+      videoRef.current.pause();
+    } else {
+      videoRef.current.play();
+    }
+  }
+
+  return (
+    <section className='app__video'>
+      <video 
+        src={meal} 
+        ref={videoRef} 
+        type="video/mp4" 
+        loop 
+        controls={false} 
+        muted 
+      />
+      <div className='app__video-overlay flex__center'>
+        <div 
+          className='app__video-overlay_circle flex__center'
+          onClick={handleVideo}
+        >
+          {playVideo 
+            ? <BsPauseFill color='#fff' fontSize={30} />
+            : <BsFillPlayFill color='#fff' fontSize={30} />
+          }
+        </div>
+      </div>
+    </section>
+  )
+};
 
 export default Intro;
